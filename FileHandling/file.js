79f888904a0d156bebbfcffe6c6297ea8dd19313
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-05-15 08:54:28 +0530

    file.js - update

--- FileHandling/file.js
+++ FileHandling/file.js
@@ -3,11 +3,33 @@ const fs = require("fs");
 // sync,,,
 // fs.writeFileSync("./test.txt", "Hey there !")
 
-//Async 
-fs.writeFile("./test.txt", "Hello World!", (err)=>{
+// //Async 
+// fs.writeFile("./test.txt", "Hello World!", (err)=>{
 
-})
+// })
 
-const result = fs.readFileSync("./contacts.txt","utf-8");
+// //Sync
+// const result = fs.readFileSync("./contacts.txt","utf-8");
 
-console.log(result);
+// //Async
+// fs.readFile("./contacts.txt", "utf-8", (err,result)=>{
+//     if(err){
+//         console.log("Error", err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// })
+
+// console.log(result);
+
+// fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+
+// fs.appendFileSync("./test.txt", `\n Hey There`);
+
+
+// copy a file
+// fs.cpSync('./test.txt','./copy.txt');
+
+// delete a file
+// fs.unlinkSync("./test.txt")
